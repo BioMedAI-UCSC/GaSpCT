bfc582c3e7ec46b8f9e16bd3b3b3aaf72e8fd7f4
Manolis Nikolakakis <manolis-nikolakakis@hotmail.com>
2024-01-02 18:56:17 -0800

    Updated script structure

--- utils/camera_generator.py
+++ utils/camera_generator.py
@@ -2,45 +2,59 @@ from math import sin, cos, asin, acos, pi
 from argparse import ArgumentParser
 import os
 import pandas as pd
+import yaml
 
 def coordinates():
     parser = ArgumentParser("Coordinate Generator")
-    parser.add_argument("--camera_extrinsic", default=1, type=float)
-    parser.add_argument("--camera_intrinsic", default=1, type=float)
-    parser.add_argument("--views", "-v", required=True, type=int)
+    parser.add_argument("--distance_source_to_patient", "-p", default=1, type=float)
+    parser.add_argument("--distance_source_to_detector", "-d", default=1, type=float)
+    parser.add_argument("--detector_size", "-s", default=1, type=float)
+    parser.add_argument("--number_of_views", "-v", required=True, type=int)
     parser.add_argument("--output", "-o", default="cameras", type=str)
 
     args = parser.parse_args()
 
-    assert args.camera_extrinsic > 0, f"camera_extrinsic must be greater than 0, got {args.camera_extrinsic}"
-    assert args.camera_intrinsic > 0, f"camera_intrinsic must be greater than 0, got {args.camera_intrinsic}"
-    assert args.views > 0, f"views must be greater than 0, got {args.views}"
+    assert args.distance_source_to_patient > 0, f"distance of source to patient must be greater than 0, got {args.distance_source_to_patient}"
+    assert args.distance_source_to_detector > 0, f"distance of source to detector must be greater than 0, got {args.distance_source_to_detector}"
+    assert args.detector_size > 0, f"detector size must be greater than 0, got {args.detector_size}"
+    assert args.number_of_views > 0, f"views must be greater than 0, got {args.number_of_views}"
 
     os.makedirs(args.output, exist_ok=True)
 
-    r = args.camera_extrinsic
+    r = int(args.distance_source_to_detector/2)
     coordinate_list = list()
 
+    # Retrieve camera poses for each of the image
     for i in range(args.views):
-        x = r * cos(2 * pi * i / args.views)
-        z = r * sin(2 * pi * i / args.views)
-        phi = 2 * pi * i / args.views + pi if i < args.views / 2 else 2 * pi * i / args.views - pi
+        x = r * cos(2 * pi * i / args.number_of_views)
+        z = r * sin(2 * pi * i / args.number_of_views)
+        phi = 2 * pi * i / args.number_of_views + pi if i < args.number_of_views / 2 else 2 * pi * i / args.number_of_views - pi
 
         coords = (x, 0, z, 0, phi)
         
         coordinate_list.append(coords)
 
-    file_text = f"Extrinsic: {r}\nIntrinsic: {args.camera_intrinsic}\n"
+    return coordinate_list
 
-    with open(args.output + "/cameras.txt", "w") as file:
-        file.write(file_text + str(coordinate_list))
+def pose_to_extrinsics(poses):
+    translation_vector = [0, 0, 0]
+    rotation_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    return translation_vector, rotation_matrix
 
-def coords_to_quaternion(coordinates, intrinsic, point_cloud):
-    points_in_view = []
+def metadata_to_intrinsics(d_source, d_patient, det_size):
+    focal_length = 0
+    principle_point = [0,0,0]
+    return focal_length, principle_point
 
-    
+def write_yaml(camera_extrinsics, camera_intrinsics):
+    data = dict(
+        Extrinsics = camera_extrinsics,
+        Intrinsics = camera_intrinsics,
+    )
 
-    return points_in_view
+    with open('data.yml', 'w') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
+
 
 def main():
     coordinates()
