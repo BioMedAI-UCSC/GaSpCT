a5cc3bf7403118344cfed2504e19fe0877703630
Manolis Nikolakakis <manolis-nikolakakis@hotmail.com>
2024-01-04 17:40:06 -0800

    first reasonable yaml file output

--- utils/camera_generator.py
+++ utils/camera_generator.py
@@ -1,4 +1,4 @@
-from math import sin, cos, asin, acos, pi
+from math import sin, cos, asin, acos, pi, radians
 from scipy.spatial.transform import Rotation as R
 from argparse import ArgumentParser
 import numpy as np
@@ -38,20 +38,19 @@ def rotation_matrix_to_quaternion(R):
     q *= 0.5 / np.sqrt(t * R[3,3])
     return q
 
-def retrieve_rotation_matrices(angle):
-    yaw = radians(angle)
+def retrieve_rotation_matrix(angle):
+    yaw =radians(angle)
     roll = 0
     pitch = 0
-    R_x = [[cos(yaw), -sin(yaw), 0], [sin(yaw), cos(yaw), 0], [0, 0, 1]]]
-    R_y = [[cos(roll), 0, sin(roll)], [0, 1, 0], [-sin(roll), 0, cos(roll)]]
-    R_z = [[1, 0 , 0], [0, cos(pitch), -sin(pitch)], [0, sin(pitch), cos(pitch)]]
+    R_x = np.array([[cos(yaw), -sin(yaw), 0], [sin(yaw), cos(yaw), 0], [0, 0, 1]])
+    R_y = np.array([[cos(roll), 0, sin(roll)], [0, 1, 0], [-sin(roll), 0, cos(roll)]])
+    R_z = np.array([[1, 0 , 0], [0, cos(pitch), -sin(pitch)], [0, sin(pitch), cos(pitch)]])
     R = np.dot(R_x, R_y, R_z)
     return R
 
-def pose_to_extrinsics(poses):
-    translation_vector = [0, 0, 0]
-    rotation_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    return translation_vector, rotation_matrix
+def calculate_translation_vector(R, C):
+    translation_vector = np.dot(np.linalg.inv(-R.transpose()), C)
+    return translation_vector
 
 def metadata_to_intrinsics(d_source, d_patient, det_size):
     focal_length = 0
@@ -65,12 +64,13 @@ def write_yaml(yaml_data):
 
 if __name__ == "__main__":
 
+    # Note to do: add required=True after finishing with tests
     parser = ArgumentParser("Coordinate Generator")
     parser.add_argument("--distance_source_to_patient", "-p", default=541, type=float)
     parser.add_argument("--distance_source_to_detector", "-d", default=949, type=float)
     parser.add_argument("--detector_size", "-s", default=350, type=float)
-    parser.add_argument("--number_of_views", "-v", default = 360, required=True, type=int)
-    parser.add_argument("--height", "-h", default = 128, type=float)
+    parser.add_argument("--number_of_views", "-v", default = 360, type=int)
+    parser.add_argument("--height", "-he", default = 128, type=float)
     parser.add_argument("--width", "-w", default = 128, type=int)
     parser.add_argument("--output", "-o", default="cameras", type=str)
 
@@ -80,23 +80,40 @@ if __name__ == "__main__":
     assert args.distance_source_to_detector > 0, f"distance of source to detector must be greater than 0, got {args.distance_source_to_detector}"
     assert args.detector_size > 0, f"detector size must be greater than 0, got {args.detector_size}"
     assert args.number_of_views > 0, f"views must be greater than 0, got {args.number_of_views}"
-    assert args.detector_size > 0, f"detector size must be greater than 0, got {args.detector_size}"
-    assert args.number_of_views > 0, f"views must be greater than 0, got {args.number_of_views}"
+    assert args.width > 0, f"detector size must be greater than 0, got {args.width}"
+    assert args.height > 0, f"views must be greater than 0, got {args.height}"
 
     os.makedirs(args.output, exist_ok=True)
 
     r = int(args.distance_source_to_detector/2)
 
-    yaml_data = {}
+    yaml_data = []
 
     for i in range(args.number_of_views):
         coords = coordinates()
         entry_name = "image_" + str(i+1)
-        Rot = retrieve_rotation_matrices(coords[4])
-        yaml_data[entry_name]['intrinsics']['focal_length'] = args.distance_source_to_patient
-        yaml_data[entry_name]['intrinsics']['principle_point'] = []
-        yaml_data[entry_name]['extrinsics']['qvec'] = R.as_quat(Rot)
-        yaml_data[entry_name]['extrinsics']['tvec'] = []
-        yaml_data[entry_name]['height'] = args.height
-        yaml_data[entry_name]['width'] = args.width
+        Rot = retrieve_rotation_matrix(coords[0][4])
+        tvec = calculate_translation_vector(Rot, coords[0][1:4])
+
+        entry = {
+            entry_name : [
+                {
+                    'intrinsics': {
+                        'focal_length': args.distance_source_to_patient,
+                        'principle_point': int(args.detector_size/2)
+                    }, 
+                    'extrinsics': {
+                        #'qvec': R.as_quat(Rot),
+                        'qvec': Rot.tolist(),
+                        'tvec': tvec.tolist()
+                    },
+                    'height': args.height,
+                    'width': args.width
+                }
+            ]
+        }
+        yaml_data.append(entry)
+
+        #import pdb; pdb.set_trace()
+
     write_yaml(yaml_data)
