e1fcbae4cf217f522cdc508734cadb20f7a70f0f
Manolis Nikolakakis <manolis-nikolakakis@hotmail.com>
2024-01-15 13:57:55 -0800

    added beta_loss

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -82,6 +82,7 @@ class OptimizationParams(ParamGroup):
         self.percent_dense = 0.01
         self.lambda_dssim = 0.2
         self.lambda_tv = 0.05
+        self.lambda_beta = 0.1
         self.densification_interval = 100
         self.opacity_reset_interval = 3000
         self.densify_from_iter = 500
--- train.py
+++ train.py
@@ -12,7 +12,7 @@
 import os
 import torch
 from random import randint
-from utils.loss_utils import l1_loss, ssim, tv_loss
+from utils.loss_utils import l1_loss, ssim, tv_loss, beta_loss
 from gaussian_renderer import render, network_gui
 import sys
 from scene import Scene, GaussianModel
@@ -90,7 +90,8 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         gt_image = viewpoint_cam.original_image.cuda()
         Ll1 = l1_loss(image, gt_image)
         Ltv = tv_loss(image)
-        loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image)) + opt.lambda_tv * Ltv
+        Lbeta = beta_loss(image)
+        loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image)) + opt.lambda_tv * Ltv + opt.lambda_beta * Lbeta
         loss.backward()
 
         iter_end.record()
