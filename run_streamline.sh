3d37774eb5854be02bd36f815367ae17ea3b9810
Manolis Nikolakakis <manolis-nikolakakis@hotmail.com>
2024-02-08 17:52:12 -0800

    changed moving images to copying images

--- run_streamline.sh
+++ run_streamline.sh
@@ -28,11 +28,11 @@ for i in $listVar; do
     cd ../../
     mv utils/$output_yaml $prefix_var$i"/sparse/0/"$output_yaml 
     # maybe we need to convert to rgb from grayscale
-    mv utils/"${prefix_dir}${i}"/*.png "${prefix_var}${i}/images/"
+    cp utils/"${prefix_dir}${i}"/*.png "${prefix_var}${i}/images/"
     for j in $test_holdout_list; do
         echo "Holding every $jth image out for testing..."
         output_name=$prefix_var$i"/ratio_"$j
-        python train.py -s ${DATA/DIR} --test_holdout $j --eval --use_yaml --name $output_name --iterations 35000 > $output_name"_train.txt"
+        python train.py -s $prefix_dir$i --test_holdout $j --eval --use_yaml --name $output_name --iterations 35000 > $output_name"_train.txt"
         python render.py -m $output_name > $output_name"_render.txt"
         python metrics.py -m $output_name > $output_name"_metrics.txt"
     done
