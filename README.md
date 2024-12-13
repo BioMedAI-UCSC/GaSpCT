5653b2c330e6056b2c36941329e9fef7871382bc
Manolis Nikolakakis <manolis-nikolakakis@hotmail.com>
2024-12-12 16:14:57 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # GaSpCT: Gaussian splatting for novel brain CBCT projection view synthesis
 
 ## Overview
-GaSpCT is an adaptation of Gaussian Splatting specifically designed for CT projection images. This project extends the original work by INRIA to handle medical imaging applications.
+GaSpCT is an adaptation of Gaussian Splatting specifically designed for sparse-view Cone Beam Computed Tomography (CBCT) projection images. This project extends the original work by INRIA to handle medical imaging applications.
+Our main contributions include:
+- A framework to extract camera intrinsics and extrinsics using scanner metadata
+- An enhanced loss function that better captures the characteristics of radiographs
+- An ellipsoid point initialization
+- A validation that Gaussian Splatting represents a robust and accurate methodology for radiograph synthesis in sparse-view CBCT
 
 ## Original Work
 This repository is heavily based on the Gaussian Splatting repository by INRIA:
@@ -11,7 +16,6 @@ This repository is heavily based on the Gaussian Splatting repository by INRIA:
 ## Project Status
 This project has been accepted for a poster presentation in SPIE Medical Imaging 2025, in San Diego
 
-## Team
 
 ### Main Contributors
 - Emmanouil "Manolis" Nikolakakis
@@ -26,4 +30,38 @@ This project has been accepted for a poster presentation in SPIE Medical Imaging
 University of California, Santa Cruz
 
 ---
-*More details about implementation, usage, and results to be added.*
+### Running the code
+
+# Cloning the Repository
+
+The repository contains submodules, thus please check it out with
+
+```bash
+# SSH
+git clone git@github.com:BioMedAI-UCSC/GaSpCT.git --recursive
+```
+or
+
+```bash
+# HTTPS
+git clone https://github.com/BioMedAI-UCSC/GaSpCT.git --recursive
+```
+
+# Installing requirements
+
+# Installing the submodules
+
+# Setting up the directory
+
+# Training, rendering and evaluating the gaussian splatting scene representation
+
+# BibTex
+
+If you have used our work or found it insightful for your project, please consider citing us using the following BibTex block
+
+@article{nikolakakis2024gaspct,
+  title={GaSpCT: Gaussian Splatting for Novel CT Projection View Synthesis},
+  author={Nikolakakis, Emmanouil and Gupta, Utkarsh and Vengosh, Jonathan and Bui, Justin and Marinescu, Razvan},
+  journal={arXiv preprint arXiv:2404.03126},
+  year={2024}
+}
--- run_streamline.sh
+++ run_streamline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# this script assumes we have a directory of multiple simulations with images and text files with projection matrices
-# it will create a new directory for each simulation and run the train.py script with different test_holdout values
-# it will then render the results and calculate the metrics for each simulation
+# This script assumes we have a directory of multiple simulations with images and text files with projection matrices
+# It will create a new directory for each simulation and run the train.py script with different test_holdout values
+# It will then render the results and calculate the metrics for each simulation
 
 echo "Starting scipt..."
 
@@ -11,9 +11,15 @@ prefix_dir="data/"
 suffix_output="_output"
 input_yaml="camera_parameters.yml"
 output_yaml="cam_config.yaml"
-listVar="simulation_0"
+
+# List of directories with radiograph images, can be multiple directories separated by space
+data_paths="simulation_0"
+# One out of how many pictures should be saved for testing, negatives indicate more testing than training
+# In the example below, 1 out of "2" images are hidden from the optimization for testing purposes
+# Multiple ratios can be passed separated by space (TODO: maybe change value to ratio?)
 test_holdout_list="2"
 
+# Hyperparameters related to the training of the model
 feature_lr=0.01
 opacity_lr=0.01
 percent_dense=0.005
@@ -22,18 +28,20 @@ lambda_tv=0.005
 lambda_beta=0.005
 iterations=25000
 
-for i in $listVar; do
+# Multiple tests for different data paths
+for i in $data_paths; do
     echo "Processing $i"
     mkdir -p output/${prefix_var}${i}/sparse/0 output/${prefix_var}${i}/images
     cp "${prefix_dir}${i}"/$input_yaml output/$prefix_var$i"/sparse/0/"$output_yaml
-    # maybe we need to convert to rgb from grayscale
     cp "${prefix_dir}${i}"/*.png "output/${prefix_var}${i}/images/"
+    # In case images need to be renamed and converted to rgb from grayscale (TODO: confirm naming works well)
     python utils/rename_images.py -i "output/${prefix_var}${i}/images/"
     python utils/from_g_to_rgb.py -i "output/${prefix_var}${i}/images/"
+    # Multiple tests for different training to testing ratios
     for j in $test_holdout_list; do
         echo "Holding every ${j}th image out for testing..."
         output_name=output/${prefix_var}${i}/ratio${j}
-	mkdir -p ${output_name}
+	    mkdir -p ${output_name}
         python train.py -s output/$prefix_var$i --test_holdout $j --eval --use_yaml --name ${prefix_var}${i}/ratio${j} --iterations $iterations --feature_lr $feature_lr --opacity_lr $opacity_lr --percent_dense $percent_dense --lambda_dssim $lambda_dssim --lambda_tv $lambda_tv --lambda_beta $lambda_beta > $output_name"/train.txt"
         python render.py -m $output_name > $output_name"/render.txt"
         python metrics.py -m $output_name > $output_name"/metrics.txt"
